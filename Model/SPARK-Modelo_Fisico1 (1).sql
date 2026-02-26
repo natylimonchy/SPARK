08e866357feff267148fd88f7000b78d80e3b2dd
Gerson <gerson.dallyn@gmail.com>
2026-02-26 20:27:00 +0100

    Create structure files MVC

---
+++ Model/SPARK-Modelo_Fisico1 (1).sql
@@ -0,0 +1,42 @@
+/*
+Natalia Amaya, Gerson Paredes, Abril Graña
+5/12/2025
+Se crea la base de datos Spark con tres tablas. Usuario almacena la información de cada usuario y permite identificar su organizador mediante una relación consigo misma. 
+Evento guarda los datos de cada evento y Asisten actúa como tabla intermedia registrando qué usuarios participan en cada evento.
+ */
+
+create database spark;
+use spark;
+
+CREATE TABLE Usuario (
+    Nombre_Usuario VARCHAR(50) PRIMARY KEY,
+    Telefono INT UNIQUE,
+    Contraseña VARCHAR(20),
+    Nombre_Apellido VARCHAR(50),
+    Correo VARCHAR(100) UNIQUE,
+    Direccion VARCHAR(50),
+    Id_organizador VARCHAR(50),
+
+    CONSTRAINT fk_organizador 
+    FOREIGN KEY (Id_organizador) 
+    REFERENCES Usuario(Nombre_Usuario)
+);
+
+CREATE TABLE Evento(
+    Id_Evento INT PRIMARY KEY,
+    Descripcion VARCHAR(100),
+    Fecha_evento DATE,
+    Ubicacion VARCHAR(50)
+);
+
+CREATE TABLE Asisten (
+    Id_Asistencia INT PRIMARY KEY AUTO_INCREMENT,
+    Nombre_Usuario VARCHAR(50),
+    Id_Evento INT,
+
+    CONSTRAINT fk_asisten_usuario
+    FOREIGN KEY (Nombre_Usuario) REFERENCES Usuario(Nombre_Usuario),
+
+    CONSTRAINT fk_asisten_evento
+    FOREIGN KEY (Id_Evento) REFERENCES Evento(Id_Evento)
+);
