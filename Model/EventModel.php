b311584ab861f21a0d8ac049dbe89d3ac8ecbfb2
Gerson <gerson.dallyn@gmail.com>
2026-05-09 16:26:31 +0200

    Implement update and delete event methods in EventModel; enhance error handling and validation #23

--- Model/EventModel.php
+++ Model/EventModel.php
@@ -20,9 +20,48 @@ class EventModel {
     public function getEventById() {}
        
 
-    public function updateEvent() {}
+    public function updateEvent($Id_Evento, $nombre, $descripcion, $fecha_evento, $ubicacion ) {
+        if(empty($Id_Evento) || empty($nombre) || empty($descripcion) || empty($fecha_evento) || empty($ubicacion)) {
+            return "campos_vacios";
+        } else {
+            try {
+                $sql = "UPDATE Evento SET Nombre = ?, Descripcion = ?, Fecha_Evento = ?, Ubicacion = ? WHERE Id_Evento = ?";
+                $stmt = $this->conn->prepare($sql);
+                $stmt->execute([$nombre, $descripcion, $fecha_evento, $ubicacion, $Id_Evento]);
+                $rowCount = $stmt->rowCount();
+                if ($rowCount === 0) {
+                    return "evento_no_encontrado";
+                }
+                return "ok";
+            } catch (PDOException $e) {
+                error_log("DEBUG Model: Exception capturada en updateEvent: " . $e->getMessage());
+                return "error_base_datos";
+            }
+        }
+                        
+    }
 
 
-    public function deleteEvent() {}
+    public function deleteEvent($Id_Evento) {
+       
+           // Implementation for deleting event
+        if (empty($Id_Evento)) {
+            return "id_vacio";
+        }else {
+            try {
+                $sql = "DELETE FROM Evento WHERE Id_Evento = ?";
+                $stmt = $this->conn->prepare($sql);
+                $stmt->execute([$Id_Evento]);
+                $rowCount = $stmt->rowCount();
+                if ($rowCount === 0) {
+                    return "evento_no_encontrado";
+                }
+                return "ok";
+            } catch (PDOException $e) {
+                error_log("DEBUG Model: Exception capturada en deleteEvent: " . $e->getMessage());
+                return "error_base_datos";
+            }
+        }
+    }
 }
 ?>
