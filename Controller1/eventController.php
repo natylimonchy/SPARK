eea741747bc631d0b763d31b8cb7692947fbb4f2
Gerson <gerson.dallyn@gmail.com>
2026-05-09 15:31:44 +0200

    Add EventModel and EventController with basic structure; create event registration view #23

---
+++ Controller1/eventController.php
@@ -0,0 +1,30 @@
+<?php
+
+
+require_once("../Model/EventModel.php");
+
+class EventController {
+    
+    private $model;
+
+    public function __construct() {
+        $this->model = new EventModel();
+    }
+
+ public function registerEvent() {
+        
+}
+
+public function getEvents() {
+   
+}
+
+
+public function updateEvent() {
+   
+}
+public function deleteEvent() {
+
+    }
+}
+?>
